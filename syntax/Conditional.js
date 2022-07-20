b9504b4daad934edfccbfe9f1bdedb110f934108
wlgus4988 <wlgus4988@naver.com>
2022-07-20 13:58:22 +0900

    220720 ~ch.25.3

---
+++ syntax/Conditional.js
@@ -0,0 +1,9 @@
+var args = process.argv;
+console.log(args[1]);
+console.log('A');
+console.log('B');
+if(false){
+  console.log('C1');
+}else{
+  console.log('C2');
+}
---
+++ syntax/Function.js
@@ -0,0 +1,11 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first,second){
+  console.log(first+second);
+}
+
+sum(2,4);
+
+console.log(Math.round(1.6));
+filwrite('')
